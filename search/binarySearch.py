4133a8b9a928475e2ac41f4d4e2f982c4218ce85
yangxin <toano0720@icloud.com>
2020-02-02 15:08:25 +0800

    Signed-off-by: yangxin <toano0720@icloud.com>

---
+++ search/binarySearch.py
@@ -0,0 +1,19 @@
+
+def binarySearch(s, k):
+    low = 0
+    high = len(s) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if s[mid] == k:
+            return mid
+        elif s[mid] > k:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
+for _ in range(int(input())):
+    s = list(map(int, input().split()))
+    k = int(input())
+    print(binarySearch(s, k))
