3d9efd2c34766ab8c3627cbede002dd2d296b4c1
toanoyx <38813080+toanoyx@users.noreply.github.com>
2020-02-12 11:50:44 +0800

    增加BFS和DFS遍历

--- tree/binaryTree.py
+++ tree/binaryTree.py
@@ -10,6 +10,7 @@ class TreeNode(Generic[T]):
         self.right = None
 
 
+# 前序遍历
 def preOrder(root: Optional[TreeNode[T]]) -> Generator[T, None, None]:
     if root:
         yield root.val
@@ -17,6 +18,7 @@ def preOrder(root: Optional[TreeNode[T]]) -> Generator[T, None, None]:
         yield from preOrder(root.right)
 
 
+# 中序遍历
 def inOrder(root: Optional[TreeNode[T]]) -> Generator[T, None, None]:
     if root:
         yield from inOrder(root.left)
@@ -24,11 +26,42 @@ def inOrder(root: Optional[TreeNode[T]]) -> Generator[T, None, None]:
         yield from inOrder(root.right)
 
 
+# 后序遍历
 def postOrder(root: Optional[TreeNode[T]]) -> Generator[T, None, None]:
     if root:
         yield from postOrder(root.left)
         yield from postOrder(root.right)
         yield root.val
+
+
+# 层次遍历（广度优先）
+def BFS(root: Optional[TreeNode[T]]):
+    if root:
+        res = []
+        queue = [root]
+        while queue:
+            currentNode = queue.pop(0)
+            res.append(currentNode.val)
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
+    return res
+
+
+# 深度优先
+def DFS(root: Optional[TreeNode[T]]):
+    if root:
+        res = []
+        stack = [root]
+        while stack:
+            currentNode = stack.pop()
+            res.append(currentNode.val)
+            if currentNode.right:
+                stack.append(currentNode.right)
+            if currentNode.left:
+                stack.append(currentNode.left)
+    return res
 
 
 if __name__ == "__main__":
@@ -51,3 +84,5 @@ if __name__ == "__main__":
     print("preOrder: " + str(list(preOrder(tree))))
     print("inOrder: " + str(list(inOrder(tree))))
     print("postOrder: " + str(list(postOrder(tree))))
+    print("BFS: " + str(list(BFS(tree))))
+    print("DFS: " + str(list(DFS(tree))))
